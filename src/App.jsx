aaf6428223adf354432ab056db332568c116912b
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2026-03-07 10:04:29 +0000

    setup: folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import TaskCard from './TaskCard';
+
 function App() {
   return (
     <>
       <h1>Journey Beyond Borders</h1>
+      <TaskCard></TaskCard>
     </>
   );
 }
---
+++ src/TaskCard.jsx
@@ -0,0 +1,12 @@
+const TaskCard = () => {
+  return (
+    <>
+      <h2>title</h2>
+      <span>Description</span>
+      <a>ResourceLink</a>
+      <button>Done</button>
+    </>
+  );
+};
+
+export default TaskCard
